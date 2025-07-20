45fb6bae3bef273edb4e3a089f4a9e377578013f
userAKAANS <besttieshello5@gmail.com>
2025-07-20 23:07:17 +0300

    Create status.js

---
+++ commands/commands/commands/commands/status.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('status')
+    .setDescription('Check the status of your order')
+    .addStringOption(opt => opt.setName('order').setDescription('Order ID').setRequired(true)),
+  async execute(interaction) {
+    const orderId = interaction.options.getString('order');
+    await interaction.reply({ content: `📦 Order #${orderId} is currently being processed. You will receive your delivery shortly.`, ephemeral: true });
+  }
+};
